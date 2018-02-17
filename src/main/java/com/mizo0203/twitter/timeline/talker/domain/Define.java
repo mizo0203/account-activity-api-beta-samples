7781c6ac9e28481300037ee39696e8818ed85301
みぞ@CrazyBeatCoder <mizo0203@mizo0203.com>
2018-02-17 23:52:58 +0900

    Remove Keys and Access Tokens Values. fixes #318 @15m

--- src/main/java/com/mizo0203/twitter/timeline/talker/domain/Define.java
+++ src/main/java/com/mizo0203/twitter/timeline/talker/domain/Define.java
@@ -1,8 +1,9 @@
 package com.mizo0203.twitter.timeline.talker.domain;
 
 public class Define {
-  public static final String CONSUMER_KEY = "ZRluO10syAzpXwrefiRjgvkP5";
-  public static final String CONSUMER_SECRET = "DOffLk1SuTPVKsdzUMAwdi3vk02MIs7YpB1WtmyIMb8RvywkG8";
-  public static final String TOKEN = "212164063-3HuBxtorV3eUsSLEoZxyWELGASYdB43VVtew6Gz1";
-  public static final String TOKEN_SECRET = "uHDdJYy4LKnpglj5sAysJuhBGtEObg6xBuWgn1KZsu0DN";
+  // TODO: Set Keys and Access Tokens
+  public static final String CONSUMER_KEY = "";
+  public static final String CONSUMER_SECRET = "";
+  public static final String TOKEN = "";
+  public static final String TOKEN_SECRET = "";
 }
